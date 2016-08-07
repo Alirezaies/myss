90683f6d2aca1f47be1fec8e931d1567b746a130
Sadegh Alirezaie <Alirezaies@users.noreply.github.com>
2016-08-07 17:26:03 +0430

    Update myss.sh
    Added Some Informations For Better UX

--- Linux/myss.sh
+++ Linux/myss.sh
@@ -3,11 +3,17 @@
 #Wrote By Sadegh Alirezaie For Those Who Like To Play On Linux Or Mac With All Love
 
 now=$(date +"%Y-%m-%d-%H:%M:%S");
+echo "Screen Capture Started At $now";
+echo "For Exit Press ' ctrl+c ' ";
+echo "======== Log ========";
 mkdir ~/Desktop/Myss/ss/$now && cd "$_";
+touch logfile.log;
 n=0;
 while [ 1 = 1 ]; do
 	n=$((n+1));
 	time=$(date +"%T");
 	scrot -d 60 '%Y-%m-%d-%H:%M:%S.jpg';
+	echo "========= $now ========" >> logfile.log;
+	tail /var/log//syslog >> logfile.log
 	echo "$n - Screenshot captured at $(date +'%T')";
 done
