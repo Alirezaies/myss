06b5be2481b2a7ad72e0a6ed27e862f5a6b785e4
Sadegh Alirezaie <Alirezaies@users.noreply.github.com>
2016-08-07 15:58:45 +0430

    Update myss.sh

--- Linux/myss.sh
+++ Linux/myss.sh
@@ -3,11 +3,18 @@
 #Wrote By Sadegh Alirezaie For Those Who Like To Play On Linux Or Mac With All Love
 
 now=$(date +"%Y-%m-%d-%H:%M:%S");
-mkdir ~/Desktop/Myss/ss/$now && cd "$_";
+startTime=$(date +"%H:%M:%S");
+mkdir ~/Desktop/Myss/$now && cd "$_";
+touch sys.log;
 n=0;
+echo "Welcome To Myss Version 3.0.0, Screen Capture Started At $startTime"
+echo " For Exit Press 'ctrl+c'";
+echo "...:::: Capture Log ::::...";
 while [ 1 = 1 ]; do
 	n=$((n+1));
 	time=$(date +"%T");
-	scrot -d 60 '%Y-%m-%d-%H:%M:%S.jpg';
+	scrot -d 3 '%Y-%m-%d-%H:%M:%S.jpg';
 	echo "$n - Screenshot captured at $(date +'%T')";
+	tail /var/log/syslog >> sys.log;
+	echo "======== Break At $startTime ========" >> sys.log;
 done
